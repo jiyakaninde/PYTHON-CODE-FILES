f40c4353828dadb11c37ec0ac3025b4611ebc267
jiyakaninde <advaykaninde@gmail.com>
2025-03-16 20:38:29 +0200

    Add files via upload

---
+++ lesson 2/activity2.py
@@ -0,0 +1,13 @@
+
+name="jiya"
+age=17
+is_student="yes"
+weight=57.5
+
+print("the data type of this is",type(name))
+print("the data type of this is",type(age))
+print("the data type of this is",type(is_student))
+print("the data type of this is",type(weight))
+
+print("edited type of this is",type(str(age)))
+print("edited type of this is",type(int(weight)))
