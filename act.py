345aa66a11d063ad0296c5530754eee3b7ae6f61
jiyakaninde <advaykaninde@gmail.com>
2025-03-18 20:53:51 +0200

    Add files via upload

---
+++ act.py
@@ -0,0 +1,9 @@
+#23x1=23
+
+for n in range(1,11):
+    print (23," X ", n," = ",23*n)
+#taking an input from the user using an input function
+
+m=int(input("enter a number"))
+for n in range(1,11):
+    print (m," X ", n," = ",m*n)
