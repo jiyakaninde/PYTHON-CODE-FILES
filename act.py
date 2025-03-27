52267b1e4d1baedcc2c07977ccb47491d03383fc
jiyakaninde <advaykaninde@gmail.com>
2025-03-27 20:58:46 +0200

    Add files via upload

--- act.py
+++ act.py
@@ -1,9 +1,28 @@
-#23x1=23
-
-for n in range(1,11):
-    print (23," X ", n," = ",23*n)
-#taking an input from the user using an input function
-
-m=int(input("enter a number"))
-for n in range(1,11):
-    print (m," X ", n," = ",m*n)
+list1 = ["Apple","Banana","Orange","Mango","Grapes"]
+#operations
+print("The fruit List is: ",list1)
+#lenght
+print(len(list1))
+#indexing
+print(list1[3])
+#slicing
+print(list1[1:4])
+#appending
+list1.append("Melon")
+print(list1)
+#pop list
+list1.pop(0)
+print(list1)
+#removing
+list1.remove("Orange")
+print(list1)
+#sort
+list2=[1,5223,5,24,2,324]
+list2.sort()
+print(list2)
+#reverse
+list2.reverse()
+print(list2)
+#delete
+list2.clear()
+print(list2)
