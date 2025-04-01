06860beb483de14ab48aede93bb8814386d0af02
jiyakaninde <advaykaninde@gmail.com>
2025-04-01 20:42:33 +0200

    Add files via upload

--- act.py
+++ act.py
@@ -1,28 +1,7 @@
-list1 = ["Apple","Banana","Orange","Mango","Grapes"]
-#operations
-print("The fruit List is: ",list1)
-#lenght
-print(len(list1))
+#creation of a tuple
+underthesea=("spongebob","Patrick","Mr.Krabs","Gary","Squidward")
+print(underthesea)
 #indexing
-print(list1[3])
+print(underthesea[0])
 #slicing
-print(list1[1:4])
-#appending
-list1.append("Melon")
-print(list1)
-#pop list
-list1.pop(0)
-print(list1)
-#removing
-list1.remove("Orange")
-print(list1)
-#sort
-list2=[1,5223,5,24,2,324]
-list2.sort()
-print(list2)
-#reverse
-list2.reverse()
-print(list2)
-#delete
-list2.clear()
-print(list2)
+print(underthesea[1:5])
