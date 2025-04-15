07ac34cac34ccc2e7795f9c1eb694fbd4ff2a0ab
jiyakaninde <advaykaninde@gmail.com>
2025-04-15 23:47:00 +0200

    Add files via upload

--- act.py
+++ act.py
@@ -1,7 +1,11 @@
-#creation of a tuple
-underthesea=("spongebob","Patrick","Mr.Krabs","Gary","Squidward")
-print(underthesea)
-#indexing
-print(underthesea[0])
-#slicing
-print(underthesea[1:5])
+class names:
+    n1="tom"
+    n2="jerry"
+    def intro(self):
+        print("introduction")
+    def display(self):
+        print("My name is ",self.n1)
+        print("and my name is ",self.n2)
+s1=names()
+s1.intro()
+s1.display()
