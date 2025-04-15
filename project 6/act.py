14814e6d9a3423bd6369a8261c3b48952e7e1d90
jiyakaninde <advaykaninde@gmail.com>
2025-04-16 00:44:21 +0200

    Add files via upload

---
+++ project 6/act.py
@@ -0,0 +1,44 @@
+#i didnt know how else to put multiple in one file sorry
+
+
+
+
+# import turtle
+# turtle.Screen().bgcolor("lavender")
+# sc=turtle.Screen()
+# sc.setup(400,300)
+# turtle.title("EQUIIIILAAAATERALLLLL TRIIIAAAANGLEEEE")
+# board=turtle.Turtle()
+# for i in range(3):
+    # board.forward(100)
+    # board.left(120)
+    # i=i+1
+# turtle.done()
+
+
+# import turtle
+# turtle.Screen().bgcolor("lavender")
+# sc=turtle.Screen()
+# sc.setup(400,300)
+# turtle.title("RECTANGLEEE")
+# board=turtle.Turtle()
+# for i in range(2):
+    # board.forward(100)
+    # board.left(90)
+    # board.forward(50)
+    # board.left(90)
+    # i=i+1
+# turtle.done
+
+import turtle
+turtle.Screen().bgcolor("lavender")
+sc=turtle.Screen()
+sc.setup(400,300)
+turtle.title("HEXAAAGOOOOOON")
+board=turtle.Turtle()
+for i in range(6):
+    board.forward(100)
+    board.left(60)
+    i=i+1
+turtle.done
+
